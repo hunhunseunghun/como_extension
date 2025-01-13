e45be4cdc7383f8ac1e6a3ffe344353c26aa9980
seunghun <hunhunseunghun@gmail.com>
2025-01-13 19:10:58 +0900

    지정가 알림 설정 ui

--- apps/chrome-extension/pages/popup/src/components/PriceNotiPopover.tsx
+++ apps/chrome-extension/pages/popup/src/components/PriceNotiPopover.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
 import { Bell } from 'lucide-react';
 
+type Status = {
+  value: string;
+  label: string;
+};
+
 export const PriceNotiPopover = () => {
+  const [selectedStatus, setSelectedStatus] = useState<Status | null>(null);
+  const [isCommandOpen, setIsCommandOpen] = useState(false); // CommandList 열림/닫힘 상태
+
+  const statuses: Status[] = [
+    { value: 'backlog', label: 'Backlog' },
+    { value: 'todo', label: 'Todo' },
+    { value: 'in progress', label: 'In Progress' },
+    { value: 'done', label: 'Done' },
+    { value: 'canceled', label: 'Canceled' },
+  ];
+
   return (
-    <div className="relative inline-flex  group">
+    <div className="relative inline-flex group">
       <Popover>
         <PopoverTrigger asChild>
           <Button variant="outline" size="icon" className="w-6 h-6 p-0 hover:cursor-pointer hover:bg-accent">
@@ -15,28 +33,46 @@ export const PriceNotiPopover = () => {
         </PopoverTrigger>
         <PopoverContent className="w-80">
           <div className="grid gap-4">
-            <div className="space-y-2">
-              <h4 className="font-medium leading-none">Dimensions</h4>
-              <p className="text-sm text-muted-foreground">Set the dimensions for the layer.</p>
-            </div>
-            <div className="grid gap-2">
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="width">Width</Label>
-                <Input id="width" defaultValue="100%" className="col-span-2 h-8" />
+            <Command>
+              <div>{selectedStatus ? selectedStatus.label : 'Coin'}</div>
+              <CommandInput
+                placeholder="Search coin"
+                onFocus={() => setIsCommandOpen(true)} // 포커스 시 열기
+                onBlur={() => setIsCommandOpen(false)} // 포커스 해제 시 닫기
+              />
+              {isCommandOpen && ( // 조건부 렌더링
+                <CommandList>
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup>
+                    {statuses.map(status => (
+                      <CommandItem
+                        key={status.value}
+                        value={status.value}
+                        onSelect={value => {
+                          setSelectedStatus(statuses.find(priority => priority.value === value) || null);
+                          setIsCommandOpen(false); // 선택 시 닫기
+                        }}>
+                        {status.label}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              )}
+              <div className="space-y-2">
+                <h4 className="font-medium leading-none">Dimensions</h4>
+                <p className="text-sm text-muted-foreground">Set the dimensions for the layer.</p>
               </div>
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="maxWidth">Max. width</Label>
-                <Input id="maxWidth" defaultValue="300px" className="col-span-2 h-8" />
+              <div className="grid gap-2">
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="width">Width</Label>
+                  <Input id="width" defaultValue="100%" className="col-span-2 h-8" />
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="maxWidth">Max. width</Label>
+                  <Input id="maxWidth" defaultValue="300px" className="col-span-2 h-8" />
+                </div>
               </div>
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="height">Height</Label>
-                <Input id="height" defaultValue="25px" className="col-span-2 h-8" />
-              </div>
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="maxHeight">Max. height</Label>
-                <Input id="maxHeight" defaultValue="none" className="col-span-2 h-8" />
-              </div>
-            </div>
+            </Command>
           </div>
         </PopoverContent>
       </Popover>
