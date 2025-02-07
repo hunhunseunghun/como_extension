50f5f5db4952d6039b71d4a948187786f44a8de2
seunghun <hunhunseunghun@gmail.com>
2025-02-07 12:34:30 +0900

    chart.js 테스트

--- apps/chrome-extension/pages/popup/src/columns/upbitColumns.tsx
+++ apps/chrome-extension/pages/popup/src/columns/upbitColumns.tsx
@@ -5,6 +5,7 @@ import { WarningIcon, CautionIcon } from '@/components/ui/warningIcon';
 import { getRegExp } from 'korean-regexp';
 import FlashCell from '@/components/FlashCell';
 import ChartToolTip from '@/components/ChartToolTip';
+import ChartCell from '@/components/ChartCell';
 
 export const getUpbitColumns = (
   coinNameKR: boolean,
@@ -104,45 +105,6 @@ export const getUpbitColumns = (
     cell: ({ getValue, row, cell }) => {
       const valueKRW = getValue() as number;
       const changeRateKRW = exchangeRateUSD > 0 ? valueKRW / exchangeRateUSD : 0;
-      // switch (exchangeMarketType) {
-      //   case 'KRW':
-      //     return (
-      //       <FlashCell
-      //         key={cell.id}
-      //         flashKey={cell.id}
-      //         bidAskStatus={row.original.ask_bid ? row.original.ask_bid : ''}
-      //         className={'flex flex-col items-end font-medium'}>
-      //         <span>{valueKRW?.toLocaleString()}</span>
-      //         <span key={exchangeRateUSD} className="text-[10px] text-gray-500">
-      //           {exchangeRateUSD > 0 &&
-      //             `$${changeRateKRW.toLocaleString('en-US', {
-      //               minimumFractionDigits: 2,
-      //               maximumFractionDigits: 2,
-      //             })}`}
-      //         </span>
-      //       </FlashCell>
-      //     );
-      //   case 'BTC':
-      //     return (
-      //       <FlashCell
-      //         key={cell.id}
-      //         flashKey={cell.id}
-      //         bidAskStatus={row.original.ask_bid ? row.original.ask_bid : ''}
-      //         className={'flex flex-col items-end font-medium'}>
-      //         <span>{valueKRW.toFixed(8)}</span>
-      //       </FlashCell>
-      //     );
-      //   case 'USDT':
-      //     return (
-      //       <FlashCell
-      //         key={cell.id}
-      //         flashKey={cell.id}
-      //         bidAskStatus={row.original.ask_bid ? row.original.ask_bid : ''}
-      //         className={'flex flex-col items-end font-medium'}>
-      //         <span>${valueKRW.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-      //       </FlashCell>
-      //     );
-      // }
 
       const priceContent = (
         <FlashCell
@@ -304,5 +266,17 @@ export const getUpbitColumns = (
       }
     },
     enableHiding: false,
+  },
+  {
+    accessorKey: 'candlestic_chart_30d',
+    id: 'candlestic_chart_30d',
+    header: () => (
+      <div className="flex justify-end font-bold">
+        <span>차트(30일/일봉)</span>
+      </div>
+    ),
+    cell: ({ row }) => {
+      return <ChartCell symbol={row.original.market} exchange="upbit" />;
+    },
   },
 ];
