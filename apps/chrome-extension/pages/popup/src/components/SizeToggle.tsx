8ef6a9fed82a462f4a8bbf21254f565e8d636c4f
seunghun <hunhunseunghun@gmail.com>
2025-02-25 02:49:33 +0900

    사이즈 토글 아이콘 수정 :)

--- apps/chrome-extension/pages/popup/src/components/SizeToggle.tsx
+++ apps/chrome-extension/pages/popup/src/components/SizeToggle.tsx
@@ -15,7 +15,7 @@ export function SizeToggle({
       className="relative hover:cursor-pointer hover:bg-accent size-6 min-w-6 border-1 group"
       variant="outline"
       onClick={switchSize}>
-      {wideSize ? <Maximize size={14} strokeWidth={2} /> : <Minimize size={14} strokeWidth={2} />}
+      {wideSize ? <Minimize size={14} strokeWidth={2} /> : <Maximize size={14} strokeWidth={2} />}
       <span className="absolute left-1/2 -translate-x-[80%] top-full mt-2 hidden w-max px-2 py-1 text-xs text-white bg-black # rounded-md opacity-0 group-hover:block group-hover:opacity-90 transition-opacity">
         {wideSize ? 'Minimize toggle' : 'Maximize Toggle'}
       </span>
--- apps/chrome-extension/pages/popup/src/components/UpdateNoteToggle.tsx
+++ apps/chrome-extension/pages/popup/src/components/UpdateNoteToggle.tsx
@@ -15,10 +15,10 @@ export const UpdateNoteToggle = ({ updatedVersion }: { updatedVersion: string })
       className="relative hover:cursor-pointer hover:bg-accent size-6 min-w-6 border-1 group"
       variant="outline"
       onClick={updateNoteHandler}>
-      <NotebookText size={14} strokeWidth={2} className={`${updatedVersion?.length && 'text-red-500'}`} />
+      <NotebookText size={14} strokeWidth={2} className={`${updatedVersion?.length && 'text-yellow-500'}`} />
       <span
-        className={`absolute left-1/2 -translate-x-1/2 top-full mt-2 hidden w-max px-2 py-1 text-xs text-white bg-black rounded-md opacity-50 group-hover:block group-hover:opacity-90 transition-opacity`}>
-        Update note
+        className={`absolute left-1/2 -translate-x-1/2 top-full mt-2 hidden w-max px-2 py-1 text-xs text-white ${updatedVersion?.length ? 'bg-yellow-500' : 'bg-black'} rounded-md opacity-50 group-hover:block group-hover:opacity-90 transition-opacity`}>
+        {updatedVersion?.length ? `Check ver.${updatedVersion}` : 'Update note'}
       </span>
     </Toggle>
   );
